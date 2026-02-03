fc2d4f42eb4c007cdd5ee330fa7ab0ef91ef44e0
marwaneTRK <marwantraiki@gmail.com>
2026-02-03 12:05:38 +0100

    jwt done

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,3 +7,29 @@ const authMiddleware = (req, res, next) => {
     req.userId = "user_123"; next();
 };
 module.exports = authMiddleware;
+
+
+// const jwt = require("jsonwebtoken");
+
+// const authMiddleware = (req, res, next) => {
+//     const authHeader = req.headers.authorization;
+
+//     if (!authHeader) {
+//         return res.status(401).json({ error: "Token manquant" });
+//     }
+
+//     const token = authHeader.split(" ")[1];
+
+//     try {
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//         // real user id should come from token
+//         req.userId = decoded.userId;
+
+//         next();
+//     } catch (error) {
+//         return res.status(401).json({ error: "Token invalide" });
+//     }
+// };
+
+// module.exports = authMiddleware;
