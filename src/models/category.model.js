c22c34a2451b7f813ae66c0e684b6e36ea9170a8
marwaneTRK <marwantraiki@gmail.com>
2026-02-03 15:39:30 +0100

    new models

---
+++ src/models/category.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    products: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    }]
+}, { timestamps: true });
+module.exports = mongoose.model("Category", categorySchema);
