750eaa3e48aff360b8ac160333fe155413f8c83e
marwaneTRK <marwantraiki@gmail.com>
2026-02-03 12:47:09 +0100

    post order route

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -3,5 +3,20 @@ const router = express.Router();// Import du middleware et du contrôleur
 const authMiddleware = require("../middlewares/auth.middleware");
 const { getMyOrders } = require("../controllers/order.controller");// Route protégée
 router.get("/my-orders", authMiddleware, getMyOrders);
-module.exports = router;
 
+router.post("/my-orders", (req, res) => {
+    const { products, shippingAddress } = req.body;
+    if (!products || !shippingAddress) {
+        return res.status(400)
+            .json({ error: "Données de commande manquantes" });
+    } res.status(201).json({
+        message: "Commande créée avec succès",
+        order: {
+            products,
+            shippingAddress
+        }
+    });
+});
+
+
+module.exports = router;
