ca221af90ff83190530730cc9bbccd73871335cf
marwaneTRK <marwantraiki@gmail.com>
2026-02-07 23:31:49 +0100

    auth update

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,35 +1,25 @@
+
+const jwt = require("jsonwebtoken");
+
 const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization;
-    if (!token) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
         return res.status(401).json({ error: "Token manquant" });
     }
-    // Simulation utilisateur extrait du token
-    req.userId = "user_123"; next();
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.userId = decoded.userId;
+
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: "Token invalide" });
+    }
 };
+
 module.exports = authMiddleware;
 
-
-// const jwt = require("jsonwebtoken");
-
-// const authMiddleware = (req, res, next) => {
-//     const authHeader = req.headers.authorization;
-
-//     if (!authHeader) {
-//         return res.status(401).json({ error: "Token manquant" });
-//     }
-
-//     const token = authHeader.split(" ")[1];
-
-//     try {
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//         // real user id should come from token
-//         req.userId = decoded.userId;
-
-//         next();
-//     } catch (error) {
-//         return res.status(401).json({ error: "Token invalide" });
-//     }
-// };
-
-// module.exports = authMiddleware;
