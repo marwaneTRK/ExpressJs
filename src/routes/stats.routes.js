6e89d93fe6063dd71e4c5e7b9b68fb89c8ef158c
marwaneTRK <marwantraiki@gmail.com>
2026-02-09 10:35:56 +0100

    stats routes

---
+++ src/routes/stats.routes.js
@@ -0,0 +1,11 @@
+stats.routes.js
+const express = require("express");
+const router = express.Router();
+const statsController = require("../controllers/stats.controller");
+
+router.get("/orders-count", statsController.getOrderCount);
+router.get("/revenue", statsController.getTotalRevenue);
+router.get("/top-products", statsController.getTopProducts);
+router.get("/orders-per-user", statsController.getOrdersPerUser);
+
+module.exports = router;
